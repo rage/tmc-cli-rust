1a9750442bf777bc5bdb9110e3e9c2210ea96aed
Juha Malinen <juha.malinen@helsinki.fi>
2021-01-25 12:21:41 +0200

    Test for command line argument --help

--- tests/command_line_test.rs
+++ tests/command_line_test.rs
@@ -1,13 +1,24 @@
-use std::process::Command;
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use std::process::Command;
 
 #[test]
-fn help() -> Result<(), Box<dyn std::error::Error>> {
+fn hello() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("tmc-cli-ohtuprojekti")?;
     cmd.assert()
         .success()
         .stdout(predicate::str::contains("Hello"));
+
+    Ok(())
+}
+
+#[test]
+fn help() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("tmc-cli-ohtuprojekti")?;
+    cmd.arg("--help");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Test My Code client written in Rust"));
 
     Ok(())
 }
