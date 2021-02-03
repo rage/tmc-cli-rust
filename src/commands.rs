9f20a21dcd3d48f0f55f191e06e0c33544194d6b
HoolaBoola <jaime.heikkiladias@helsinki.fi>
2021-02-03 20:49:23 +0200

    Added login command file, still empty

--- src/commands.rs
+++ src/commands.rs
@@ -1,13 +1,12 @@
-use test_command::test;
-mod test_command;
+use login_command::login;
+mod login_command;
 
 pub fn handle(matches: &clap::ArgMatches) {
-    // if the subcommand is chosen, extract it from the ArgMatches
-    if let Some(ref matches) = matches.subcommand_matches("test") {
-        // if the path to the exercise was given (which should have been), the test function is
-        // called
-        if let Some(exercise_path) = matches.value_of("exercise") {
-            test(exercise_path);
-        }
+    println!("{:#?}", matches.subcommand());
+
+    match matches.subcommand() {
+        ("login", _) => login(),
+        (_, None) => (), // No subcommand was given
+        _ => unreachable!(),
     }
 }
