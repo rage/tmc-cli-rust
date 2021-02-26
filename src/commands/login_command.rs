fe5ea06e6103907ebcc287e0250b707332ef5f3a
HoolaBoola <jaime.heikkiladias@helsinki.fi>
2021-02-26 21:36:09 +0200

    Edited login command tests. Now the IoTest struct contains an Iter over &str objects, that can act as an "input"

--- src/commands/login_command.rs
+++ src/commands/login_command.rs
@@ -35,83 +35,98 @@ pub fn login(io: &mut dyn Io, client: &mut dyn Client) {
 }
 
 #[cfg(test)]
-pub struct IoTest<'a> {
-    list: &'a mut Vec<String>,
-}
-
-#[cfg(test)]
-impl IoTest<'_> {
-    // pub fn buffer_length(&mut self) -> usize {
-    //     self.list.len()
-    // }
-
-    // pub fn buffer_get(&mut self, index: usize) -> String {
-    //     self.list[index].to_string()
-    // }
-}
-
-#[cfg(test)]
-impl Io for IoTest<'_> {
-    fn read_line(&mut self) -> String {
-        "".to_string()
-    }
-
-    fn print(&mut self, output: String) {
-        print!("{}", output);
-        self.list.push(output);
-    }
-
-    fn println(&mut self, output: String) {
-        println!("{}", output);
-        self.list.push(output);
-    }
-
-    fn read_password(&mut self) -> String {
-        "".to_string()
-    }
-}
-
-// #[cfg(test)]
-// pub struct ClientTest {
-// }
-
-// #[cfg(test)]
-// impl ClientTest {
-// }
-
-// #[cfg(test)]
-// impl Client for ClientTest {
-//     fn load_login(&mut self) -> Result<(), String>;
-//     fn try_login(&mut self, username: String, password: String) -> Result<String, String>;
-//     fn list_courses(&mut self) -> Result<Vec<Course>, String>;
-//     fn get_organizations(&mut self) -> Result<Vec<Organization>, String>;
-//     fn logout(&mut self);
-//     fn get_course_exercises(&mut self, course_id: usize) -> Result<Vec<CourseExercise>, String>;
-//     fn download_or_update_exercises(&mut self, download_params: Vec<(usize, PathBuf)>) -> Result<String, String>;
-// }
-
-#[cfg(test)]
 mod tests {
+
     use super::*;
+    use std::slice::Iter;
+    pub struct IoTest<'a> {
+        list: &'a mut Vec<String>,
+        input: &'a mut Iter<'a, &'a str>,
+    }
 
-    #[test]
-    fn empty_username_test() {
-        let mut v: Vec<String> = Vec::new();
-        let mut io = IoTest { list: &mut v };
-
-        // let mut client = ClientTest { };
-
-        assert!(io.read_line().eq(""));
-
-        // login(&mut io, &mut client);
-
-        // assert_eq!(2, io.buffer_length());
-
-        // if io.buffer_length() == 2 {
-        //     assert!(io
-        //         .buffer_get(1)
-        //         .to_string()
-        //         .eq(&"Username cannot be empty!".to_string()));
+    #[cfg(test)]
+    impl IoTest<'_> {
+        // pub fn buffer_length(&mut self) -> usize {
+        //     self.list.len()
         // }
+
+        // pub fn buffer_get(&mut self, index: usize) -> String {
+        //     self.list[index].to_string()
+        // }
+    }
+
+    #[cfg(test)]
+    impl Io for IoTest<'_> {
+        fn read_line(&mut self) -> String {
+            match self.input.next() {
+                Some(string) => string,
+                None => "",
+            }
+            .to_string()
+        }
+
+        fn print(&mut self, output: String) {
+            print!("{}", output);
+            self.list.push(output);
+        }
+
+        fn println(&mut self, output: String) {
+            println!("{}", output);
+            self.list.push(output);
+        }
+
+        fn read_password(&mut self) -> String {
+            "".to_string()
+        }
+    }
+
+    // #[cfg(test)]
+    // pub struct ClientTest {
+    // }
+
+    // #[cfg(test)]
+    // impl ClientTest {
+    // }
+
+    // #[cfg(test)]
+    // impl Client for ClientTest {
+    //     fn load_login(&mut self) -> Result<(), String>;
+    //     fn try_login(&mut self, username: String, password: String) -> Result<String, String>;
+    //     fn list_courses(&mut self) -> Result<Vec<Course>, String>;
+    //     fn get_organizations(&mut self) -> Result<Vec<Organization>, String>;
+    //     fn logout(&mut self);
+    //     fn get_course_exercises(&mut self, course_id: usize) -> Result<Vec<CourseExercise>, String>;
+    //     fn download_or_update_exercises(&mut self, download_params: Vec<(usize, PathBuf)>) -> Result<String, String>;
+    // }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn empty_username_test() {
+            let mut v: Vec<String> = Vec::new();
+            let input = vec!["moi"];
+            let mut input = input.iter();
+            let mut io = IoTest {
+                list: &mut v,
+                input: &mut input,
+            };
+
+            // let mut client = ClientTest { };
+
+            assert!(io.read_line().eq("moi"));
+
+            // login(&mut io, &mut client);
+
+            // assert_eq!(2, io.buffer_length());
+
+            // if io.buffer_length() == 2 {
+            //     assert!(io
+            //         .buffer_get(1)
+            //         .to_string()
+            //         .eq(&"Username cannot be empty!".to_string()));
+            // }
+        }
     }
 }
