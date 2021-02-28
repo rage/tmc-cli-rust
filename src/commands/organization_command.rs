31da92c5b65121c9c61c96316861a2a3258f5958
HoolaBoola <jaime.heikkiladias@helsinki.fi>
2021-03-01 01:45:53 +0200

    Added interactive mode implemented with Skim. Might not work on Windows, will have to find an answer

--- src/commands/organization_command.rs
+++ src/commands/organization_command.rs
@@ -1,10 +1,12 @@
 use super::command_util;
 use super::command_util::Client;
 use crate::io_module::Io;
+use skim::prelude::*;
+use std::io::Cursor;
 
 // Asks for organization from user and saves it into file
 pub fn set_organization(io: &mut dyn Io, client: &mut dyn Client) -> Result<String, String> {
-    // List all organizations
+    //List all organizations
     let orgs = client.get_organizations().unwrap();
     for org in &orgs {
         io.print(&org.name);
@@ -20,8 +22,37 @@ pub fn set_organization(io: &mut dyn Io, client: &mut dyn Client) -> Result<Stri
         command_util::set_organization(&slug).unwrap();
         return Ok(org.name);
     }
-
     Err(format!("No such organization for the given slug: {}", slug))
+}
+
+pub fn set_organization_interactive(client: &mut dyn Client) -> Result<String, String> {
+    let orgs = client.get_organizations().unwrap();
+
+    let options = SkimOptionsBuilder::default()
+        .height(Some("50%"))
+        .build()
+        .unwrap();
+
+    let input = orgs
+        .iter()
+        .map(|org| format!("{}", org.name))
+        .collect::<Vec<_>>();
+    let item_reader = SkimItemReader::default();
+    let items = item_reader.of_bufread(Cursor::new(input.join("\n")));
+
+    let selected_items = Skim::run_with(&options, Some(items))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(|| Vec::new());
+
+    if let Some(name) = selected_items.iter().next() {
+        let name = name.output();
+        if let Some(org) = orgs.into_iter().find(|org| org.name == name) {
+            command_util::set_organization(&org.slug).unwrap();
+            return Ok(org.name);
+        }
+    }
+
+    Err(format!("No such organization"))
 }
 
 // Check if logged in, then ask for organization
@@ -31,7 +62,7 @@ pub fn organization(io: &mut dyn Io, client: &mut dyn Client) {
         return;
     };
 
-    match set_organization(io, client) {
+    match set_organization_interactive(client) {
         Ok(org) => io.println(&format!("Selected {} as organization.", org)),
         Err(msg) => io.println(&msg),
     }
