049ed3f563c7b97908fa7b194598f9f4f728db7e
Juha Malinen <juha.malinen@helsinki.fi>
2021-03-05 16:32:37 +0200

    Unit tests for login-command

--- src/commands/login_command.rs
+++ src/commands/login_command.rs
@@ -1,5 +1,5 @@
 use super::command_util::Client;
-use super::organization_command::set_organization;
+use super::organization_command;
 use crate::io_module::Io;
 
 pub fn login(io: &mut dyn Io, client: &mut dyn Client) {
@@ -26,7 +26,7 @@ pub fn login(io: &mut dyn Io, client: &mut dyn Client) {
     match client.try_login(username, password) {
         Ok(message) => {
             io.println(&message);
-            if let Err(_err) = set_organization(io, client) {
+            if let Err(_err) = organization_command::set_organization(io, client) {
                 io.println("Could not set organization");
             }
         }
@@ -88,7 +88,6 @@ mod tests {
             list: &mut v,
             input: &mut input,
         };
-
         assert!(io.read_line().eq("test_that_buffer_for_test_input_works"));
 
         let mut mock = MockClient::new();
@@ -128,6 +127,95 @@ mod tests {
                 .buffer_get(1)
                 .to_string()
                 .eq(&"Username cannot be empty!".to_string()));
+        }
+    }
+
+    #[test]
+    fn login_with_incorrect_username_or_password_test() {
+        let mut v: Vec<String> = Vec::new();
+        let input = vec!["test_username", "test_password"];
+        let mut input = input.iter();
+        let mut io = IoTest {
+            list: &mut v,
+            input: &mut input,
+        };
+
+        let mut mock = MockClient::new();
+        mock.expect_load_login().returning(|| Err("".to_string()));
+
+        let _username = String::from("test_username");
+        let _password = String::from("test_password");
+
+        mock.expect_try_login()
+            .returning(|_username, _password| Err("error_message".to_string()));
+
+        login(&mut io, &mut mock);
+
+        assert_eq!(4, io.buffer_length());
+        if io.buffer_length() == 4 {
+            assert!(io
+                .buffer_get(3)
+                .to_string()
+                .eq(&"error_message".to_string()));
+        }
+    }
+
+    #[test]
+    fn login_with_correct_username_and_password_test() {
+        let mut v: Vec<String> = Vec::new();
+        let input = vec!["test_username", "test_password", "wrong_slug"];
+        let mut input = input.iter();
+        let mut io = IoTest {
+            list: &mut v,
+            input: &mut input,
+        };
+
+        let mut mock = MockClient::new();
+        mock.expect_load_login().returning(|| Err("".to_string()));
+
+        let _username = String::from("test_username");
+        let _password = String::from("test_password");
+
+        mock.expect_try_login()
+            .returning(|_username, _password| Ok("ok_message_for_try_login".to_string()));
+
+        mock.expect_get_organizations().returning(|| {
+            Ok(vec![
+                Organization {
+                    name: "org1".to_string(),
+                    slug: "slug_org1".to_string(),
+                },
+                Organization {
+                    name: "org2".to_string(),
+                    slug: "slug_org2".to_string(),
+                },
+            ])
+        });
+
+        login(&mut io, &mut mock);
+
+        assert_eq!(12, io.buffer_length());
+
+        for i in 0..io.buffer_length() {
+            println!("{}: {}", i, io.buffer_get(i));
+        }
+
+        if io.buffer_length() == 12 {
+            assert!(io
+                .buffer_get(3)
+                .to_string()
+                .eq(&"ok_message_for_try_login".to_string()));
+            assert!(io.buffer_get(7).to_string().eq(&"org2".to_string()));
+            assert!(io.buffer_get(8).to_string().eq(&" Slug: ".to_string()));
+            assert!(io.buffer_get(9).to_string().eq(&"slug_org2".to_string()));
+            assert!(io
+                .buffer_get(10)
+                .to_string()
+                .eq(&"\nChoose organization by writing its slug: ".to_string()));
+            assert!(io
+                .buffer_get(11)
+                .to_string()
+                .eq(&"Could not set organization".to_string()));
         }
     }
 }
