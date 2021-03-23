ea9726cb00f9a0f1d58ca9b546ecc2a04b77f8c7
HoolaBoola <jaime.heikkiladias@helsinki.fi>
2021-03-23 06:41:47 +0200

    Modified install script to work with other shells than bash, and the application should now be callable by just 'tmc' on Windows as well

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,8 +6,8 @@ echo "(If your shell is not bash, you may have to do the installation manually.)
 echo ""
 
 if (( $# < 2 )); then
-    echo "You need to give architecture (x86_64/i686) and OS (mac, linux) as arguments."
-    exit 1
+  echo "You need to give architecture (x86_64/i686) and OS (mac, linux) as arguments."
+  exit 1
 fi
 
 # Get platform-string from first argument, OS from the second
@@ -16,8 +16,8 @@ os=$2
 
 echo "Fetching latest version URL from https://download.mooc.fi"
 if ! PAGE=$(curl -s https://download.mooc.fi); then
-	echo "Failed to reach download.mooc.fi" >&2
-	exit
+  echo "Failed to reach download.mooc.fi" >&2
+  exit
 fi
 
 # Adding spaces so ${PAGE[@]} will work.
@@ -25,40 +25,41 @@ PAGE=$(echo $PAGE | sed -r 's:</Contents><Contents>:</Contents> <Contents>:g')
 
 fileprefx=""
 if [[ "$os" == "mac" ]]; then
-    fileprefx="tmc-cli-rust-${platform}-apple-darwin-v"
+  fileprefx="tmc-cli-rust-${platform}-apple-darwin-v"
 else
-    fileprefx="tmc-cli-rust-${platform}-unknown-linux-gnu-v"
+  fileprefx="tmc-cli-rust-${platform}-unknown-linux-gnu-v"
 fi
 
 
 prefx="<Key>tmc-cli-rust/$fileprefx"
 suffx="</Key>"
 
+
 regx="${prefx}[0-9]+\.[0-9]+\.[0-9]+${suffx}"
 
 # Finding the latest version of the executable
 version="0.0.0"
 for entry in ${PAGE[@]}; do
-    if [[ ${entry} =~ $regx ]]; then        
-        noprefix="${BASH_REMATCH[0]#$prefx}" #remove prefix
-        newversion="${noprefix%$suffx}" #remove suffix
-        
-        IFS=. verold=(${version##*-})
-        IFS=. vernew=(${newversion##*-})
+  if [[ ${entry} =~ $regx ]]; then        
+    noprefix="${BASH_REMATCH[0]#$prefx}" #remove prefix
+    newversion="${noprefix%$suffx}" #remove suffix
 
-        if ((${vernew[0]} > ${verold[0]} )); then
-            version=$newversion
-        elif ((${vernew[0]} >= ${verold[0]} )) && ((${vernew[1]} > ${verold[1]} )) ; then
-            version=$newversion
-        elif ((${vernew[0]} >= ${verold[0]} )) && ((${vernew[1]} >= ${verold[1]} )) && ((${vernew[2]} > ${verold[2]} )) ; then
-            version=$newversion
-        fi
+    IFS=. verold=(${version##*-})
+    IFS=. vernew=(${newversion##*-})
+
+    if ((${vernew[0]} > ${verold[0]} )); then
+      version=$newversion
+    elif ((${vernew[0]} >= ${verold[0]} )) && ((${vernew[1]} > ${verold[1]} )) ; then
+      version=$newversion
+    elif ((${vernew[0]} >= ${verold[0]} )) && ((${vernew[1]} >= ${verold[1]} )) && ((${vernew[2]} > ${verold[2]} )) ; then
+      version=$newversion
     fi
+  fi
 done
 
 if [[ $version == "0.0.0" ]]; then 
-    echo "Could not find version";
-    exit 1
+  echo "Could not find version";
+  exit 1
 fi
 echo "Latest version: $version" 
 
@@ -73,19 +74,36 @@ echo
 curl -L "$URL" > "$PWD/$filename"
 
 if [ ! -f "$PWD/$filename" ]; then
-	echo "Error downloading TMC-CLI"
-	exit 1
+  echo "Error downloading TMC-CLI"
+  exit 1
 fi
 
 # Gives execution privileges for the file
 chmod u+x "$PWD/$filename"
 
+SHELLNAME=$(basename "$SHELL")
+if [ "$SHELLNAME" = "bash" ]; then
+  PROFILEFILE=$HOME/.bashrc
+  echo "You are currently using bash as your shell, so defaulting to .bashrc for environment variables."
+elif [ "$SHELLNAME" = "zsh" ]; then
+  PROFILEFILE=$HOME/.zshrc
+  echo "You are currently using zsh as your shell, so defaulting to .zshrc for environment variables."
+elif [ "$SHELLNAME" = "csh" ] || [ "$SHELLNAME" = "tcsh" ]; then
+  PROFILEFILE=$HOME/pg-shellvariables
+  echo "This script does not automatically add variables with csh syntax to your shell configuration."
+  echo "Please add manually variables from $PROFILEFILE to your .cshrc using csh syntax."
+else
+  PROFILEFILE=$HOME/.shrc
+  echo "Defaulting to .shrc for environment variables, if this is incorrect, please copy these manually to correct file."
+fi
 # Removes old aliases
-sed -i '/alias tmc=/d' "$HOME/.bashrc"
+sed -i '/alias tmc=/d' "$PROFILEFILE"
+
+echo $PROFILEFILE
 
 # Saves new alias to .bashrc
-echo "alias tmc='$PWD/$filename'" >> "$HOME/.bashrc"
-echo "export TMC_LANGS_CONFIG_DIR='$HOME/tmc-config'" >> "$HOME/.bashrc"
+echo "alias tmc='$PWD/$filename'" >> "$PROFILEFILE"
+echo "export TMC_LANGS_CONFIG_DIR='$HOME/tmc-config'" >> "$PROFILEFILE"
 
 echo ""
 
