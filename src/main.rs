3b449f643c93b93e6e8539d3e953a465a0673d3d
Tykky <40919314+Tykky@users.noreply.github.com>
2021-05-10 20:43:38 +0300

    Fix fmt warnings

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,7 @@ fn main() {
     match matches.occurrences_of("no-update") {
         0 => {
             let os = std::env::consts::OS;
-            if os == "windows" {
-            }
+            if os == "windows" {}
         }
         _ => println!("No Auto-Updates"),
     }
